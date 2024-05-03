ce615a10f2fe535e6279c7cc08f40fc77bcedfee
agcuhjkwsl <gs1millykei@hotmail.com>
2024-05-03 17:49:38 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function sortedArrayToBST(nums) {
-  if (!nums.length) return null;
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-  return root;
+const person = { name: "Alice", age: 30 };
+if (person.hasOwnProperty("age")) {
+  console.log("The person has an age property");
 }
