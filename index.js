8bc6e19b424cf07c27761c054f064a05852caea0
agcuhjkwsl <gs1millykei@hotmail.com>
2024-05-06 11:00:49 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
+  }
+}
